91544722f5d0ceaba600218aac54b335c4aa6d3f
migi.perez <migi.perez>
2020-11-13 11:15:11 +0100

    Minor changes.

--- Abgabe1/src/main/java/Main.java
+++ Abgabe1/src/main/java/Main.java
@@ -5,11 +5,11 @@ public class Main {
 
         double startBetrag = SimpleIO.getDouble("Bitte gebe den Betrag ein.");
         double zinsSatz = SimpleIO.getDouble("Bitte gebe den Zinssatz ein.");
-        int counterFürZinsen = 0;
+        int counterFuerZinsen = 0;
 
         String ziel = SimpleIO.getString("Bitte gebe ein ob du das Ziel oder die Zeit berechnen möchtest.");
 
-        while (!(ziel.equals("Ziel")) && !(ziel.equals("Zeit"))) {
+        while (!ziel.equals("Ziel") && !ziel.equals("Zeit")) {
             SimpleIO.output("Bitte 'Ziel' oder 'Zeit' eingeben.");
             ziel = SimpleIO.getString("Bitte gebe ein ob du das Ziel oder die Zeit berechnen möchtest.");
         }
@@ -17,9 +17,9 @@ public class Main {
             double zielBetrag = SimpleIO.getDouble("Was ist ihr Zielbetrag?");
             while (startBetrag < zielBetrag) {
                 startBetrag = startBetrag * (1 + (zinsSatz / 100));
-                counterFürZinsen++;
+                counterFuerZinsen++;
             }
-            SimpleIO.output("Es dauert " + counterFürZinsen + " Jahre. Danach haben sie: " + startBetrag + " Euro.");
+            SimpleIO.output("Es dauert " + counterFuerZinsen + " Jahre. Danach haben sie: " + startBetrag + " Euro.");
         } else {
             int dauerVonJahren = SimpleIO.getInt("Wie viele Jahre wollen Sie sparen?");
             for (int i = 0; i < dauerVonJahren; i++) {
@@ -27,7 +27,5 @@ public class Main {
             }
             SimpleIO.output("Bei einem Zinssatz von: " + zinsSatz + "% und einem Startbetrag von " + startBetrag + " hat man nach " + dauerVonJahren + " Jahren " + startBetrag + " Euro gespart.");
         }
-
-
     }
 }
